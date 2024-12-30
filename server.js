2651d536c8566ab0bd02cc9fe38a4aa58ddf6745
Mohsin1016 <muhammadmohsin1016@gmail.com>
2024-12-31 02:57:39 +0500

    calculation-11

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://example.com'], 
+  origin: ['http://localhost:3000', 'https://best-rate-fron.vercel.app'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   credentials: true, 
 }));
